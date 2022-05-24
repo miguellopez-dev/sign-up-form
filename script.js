545cb7bf14e9c41bb5361749fef4323f90d915aa
Miguel Lopez <hello@miguellopez.dev>
2022-05-23 23:31:12 -0700

    Confirm password validition

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const phone = document.getElementById('tel_number');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm_password');
 
+// Adds a dash after the third and seventh number
 phone.addEventListener('keydown', (e) => {
 	if (e.key === 'Backspace' || e.key === 'Delete') return;
 	if (e.target.value.length === 3) {
@@ -8,3 +11,14 @@ phone.addEventListener('keydown', (e) => {
 		phone.value = phone.value + '-';
 	}
 });
+
+const checkPassword = function () {
+	if (password.value != confirmPassword.value) {
+		confirmPassword.setCustomValidity('Passwords do not match');
+	} else {
+		confirmPassword.setCustomValidity('');
+	}
+};
+
+password.onchange = checkPassword;
+confirmPassword.onkeyup = checkPassword;
